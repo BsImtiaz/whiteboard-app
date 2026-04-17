83b50bdf5b9b18fcae867186407e5bd9cfb0c468
BsImtiaz <bsimtiaz004@gmail.com>
2026-04-17 06:49:44 +0530

    connect backend to frontend

--- frontend/src/Whiteboard.jsx
+++ frontend/src/Whiteboard.jsx
@@ -1,7 +1,7 @@
 import { useRef, useEffect, useState } from "react";
 import io from "socket.io-client";
 
-const socket = io("http://localhost:5000");
+const socket = io("https://whiteboard-app-1-oiuz.onrender.com/");
 
 export default function Whiteboard() {
   const canvasRef = useRef();
